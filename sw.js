ab4fc49ede6e1364aa5ee894db72c5edac9dd16c
copforrsa <148183144+copforrsa@users.noreply.github.com>
2026-09-02 06:28:13 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tournoi-foot-v36-6';
+const CACHE='tournoi-foot-v36-7';
 const STATIC_ASSETS=['./','./index.html','./manifest.webmanifest','./favicon.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
