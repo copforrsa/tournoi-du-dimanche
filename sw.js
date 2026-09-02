bcff8a7a6c4f0930160018646fb0c88fa65fa0ac
copforrsa <148183144+copforrsa@users.noreply.github.com>
2026-09-02 19:10:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='swe-tournament-5v5-v39-33';
+const CACHE='swe-tournament-5v5-v39-34';
 const STATIC_ASSETS=['./','./index.html','./manifest.webmanifest','./favicon.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
